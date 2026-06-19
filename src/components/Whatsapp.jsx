7f05803248393a65bef92ab93353e1b4840fc6da
Bandaru srinivas <bandarusrinivas626@gmail.com>
2026-06-19 11:11:39 +0530

    Updated Phone number

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 function Whatsapp() {
   return (
-    <a href="https://wa.me/8317627936" className="whatsapp" target="_blank">
+    <a href="https://wa.me/+918317627936" className="whatsapp" target="_blank">
       <FaWhatsapp />
     </a>
   );
